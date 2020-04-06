d66f1e0c6f38fe91e08cb74feece86424bcdbd87
hummai <shining.melon2015@gmail.com>
2020-04-06 16:14:53 +0300

    Added cart and item classes

---
+++ src/main/java/homework/Cart.java
@@ -0,0 +1,50 @@
+package homework;
+
+import java.util.Set;
+
+public class Cart {
+    private int id;
+    private int total;
+    private String name;
+    private Set<Item> items;
+
+    public Cart() {
+    }
+
+    public Cart(String name, int total) {
+        this.name = name;
+        this.total = total;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getTotal() {
+        return total;
+    }
+
+    public void setTotal(int total) {
+        this.total = total;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<Item> getItems() {
+        return items;
+    }
+
+    public void setItems(Set<Item> items) {
+        this.items = items;
+    }
+}
